7f1b5700f424955a892adc2ac713964b3dc05569
Julio.Santos <julio.santos@parivedasolutions.com>
2016-07-18 16:26:08 -0400

    Removed express from project.  Created helloworld using koa.

--- app.js
+++ app.js
@@ -1,10 +1,1 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World test!');
-});
-
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+require('./src/helloworld/index.js');
