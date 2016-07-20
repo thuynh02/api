b688395edc59ea99a33e74ca78ed635f2a55476e
Paul Sambolin <paulsambolin@PSAMBOLIN-MBP-NY.local>
2016-07-20 14:23:11 -0400

    changed file to test codedeploy re-deployment

--- src/helloworld/hello-world.service.js
+++ src/helloworld/hello-world.service.js
@@ -3,7 +3,7 @@
 let HelloWorldModel = require("./hello-world.model");
 
 function getGreeting() {
-    return new HelloWorldModel("Hello WorldTest!").toJSON();
+    return new HelloWorldModel("Hello World!").toJSON();
 }
 
 module.exports = {
