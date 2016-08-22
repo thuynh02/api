9ed8afc7dfd52a209df64276e2fd75c146f772a0
paulSambolin <paulsambolin@gmail.com>
2016-08-22 09:18:13 -0400

    added logger.ts

---
+++ src/server/logger.ts
@@ -0,0 +1,32 @@
+var bunyan = require('bunyan');
+var createCWStream = require('bunyan-cloudwatch');
+//var dotenv = require('dotenv');
+//dotenv.load();
+
+// Sets up stream to CloudWatch
+var cloudWatchStream = createCWStream({
+  logGroupName: 'NodeLogs',
+  logStreamName: 'Dev',
+  cloudWatchLogsOptions: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
+    region: 'us-east-1'
+  },
+});
+
+var logger = bunyan.createLogger({
+  name: 'Dev',
+  level: 'info',
+  streams: [
+    {
+      stream: process.stdout
+    },
+    {
+      stream: cloudWatchStream,
+      type: 'raw'
+    }
+  ],
+  src: true
+});
+
+module.exports = logger;
