786355b4dfc66f2e1be11c49b077849b87ab88b7
paulSambolin <paulsambolin@gmail.com>
2016-08-26 14:24:45 -0400

    initialization order to secure database calls

--- app.ts
+++ app.ts
@@ -9,12 +9,17 @@ var SController = require('./src/postgres/seq.controller.js');
 // Create the server instance
 const server =  new SSServer();
 
-// Init the postgres module
-const seq = new SController(server);
-
 // Init the hello world modules
 const hello =  new HWController(server);
+
+/* 
+*  Authorization Key is currently set in private-hello-world.controller.js
+*  Move to sweet-skills-server when ready to secure all transactions
+*/
 const hellop =  new PHWController(server);
+
+// Init the postgres module
+const seq = new SController(server);
 
 // Start the server
 server.start();
