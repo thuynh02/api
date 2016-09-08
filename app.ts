d4b435676392a430f5817a709bc78b5e17e8c313
Sal Cotilletta <sal.cotilletta@parivedasolutions.com>
2016-09-08 10:29:19 -0400

    Removed postgres example module

--- app.ts
+++ app.ts
@@ -1,16 +1,12 @@
 var SSServer = require('./src/server/sweet-skills-server');
 var HWController = require('./src/helloworld/hello-world.controller');
 var PHWController = require('./src/privatehelloworld/private-hello-world.controller');
-var SController = require('./src/postgres/seq.controller');
 var SKController = require('./src/skills/skills.controller');
 
 'use strict';
 
 // Create the server instance
 const server =  new SSServer();
-
-// Init the postgres module
-const seq = new SController(server);
 
 // Init the skills module
 const skills = new SKController(server);
